1660a5d43cce50cc9153daf94ef457be340755c6
nigarofe <nigarofe@gmail.com>
2025-07-17 19:06:46 -0300

    css bugfix

--- src/sidepanelProvider.ts
+++ src/sidepanelProvider.ts
@@ -1,69 +1,82 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export class SidepanelProvider implements vscode.WebviewViewProvider {
-    public static readonly viewType = 'cal-question-view';
+  public static readonly viewType = "cal-question-view";
 
-    private _view?: vscode.WebviewView;
+  private _view?: vscode.WebviewView;
 
-    constructor(private readonly _extensionUri: vscode.Uri) {}
+  constructor(private readonly _extensionUri: vscode.Uri) {}
 
-    public resolveWebviewView(
-        webviewView: vscode.WebviewView,
-        context: vscode.WebviewViewResolveContext,
-        _token: vscode.CancellationToken
-    ) {
-        this._view = webviewView;
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ) {
+    this._view = webviewView;
 
-        webviewView.webview.options = {
-            // Allow scripts in the webview
-            enableScripts: true,
+    webviewView.webview.options = {
+      // Allow scripts in the webview
+      enableScripts: true,
 
-            localResourceRoots: [this._extensionUri]
-        };
+      localResourceRoots: [this._extensionUri],
+    };
 
-        webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
 
-        webviewView.webview.onDidReceiveMessage(data => {
-            switch (data.type) {
-                case 'questionEntered': {
-                    if (data.value) {
-                        vscode.commands.executeCommand('vscode-cal.openQuestionByNumber', parseInt(data.value));
-                    }
-                    break;
-                }
-                case 'previewQuestion': {
-                    vscode.commands.executeCommand('vscode-cal.previewQuestion');
-                    break;
-                }
-                case 'saveQuestion': {
-                    vscode.commands.executeCommand('vscode-cal.saveQuestion');
-                    break;
-                }
-                case 'registerAttemptWithoutHelp': {
-                    vscode.commands.executeCommand('vscode-cal.registerAttemptWithoutHelp');
-                    break;
-                }
-                case 'registerAttemptWithHelp': {
-                    vscode.commands.executeCommand('vscode-cal.registerAttemptWithHelp');
-                    break;
-                }
-            }
-        });
-    }
+    webviewView.webview.onDidReceiveMessage((data) => {
+      switch (data.type) {
+        case "questionEntered": {
+          if (data.value) {
+            vscode.commands.executeCommand(
+              "vscode-cal.openQuestionByNumber",
+              parseInt(data.value)
+            );
+          }
+          break;
+        }
+        case "previewQuestion": {
+          vscode.commands.executeCommand("vscode-cal.previewQuestion");
+          break;
+        }
+        case "saveQuestion": {
+          vscode.commands.executeCommand("vscode-cal.saveQuestion");
+          break;
+        }
+        case "registerAttemptWithoutHelp": {
+          vscode.commands.executeCommand(
+            "vscode-cal.registerAttemptWithoutHelp"
+          );
+          break;
+        }
+        case "registerAttemptWithHelp": {
+          vscode.commands.executeCommand("vscode-cal.registerAttemptWithHelp");
+          break;
+        }
+      }
+    });
+  }
 
-    private _getHtmlForWebview(webview: vscode.Webview) {
-        // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
-        const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "main.js")
+    );
 
-        // Do the same for the stylesheet.
-        const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css'));
-        const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css'));
-        const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'));
+    // Do the same for the stylesheet.
+    const styleResetUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
+    );
+    const styleVSCodeUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
+    );
+    const styleMainUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "main.css")
+    );
 
-        // Use a nonce to only allow a specific script to be run.
-        const nonce = getNonce();
+    // Use a nonce to only allow a specific script to be run.
+    const nonce = getNonce();
 
-        return `<!DOCTYPE html>
+    return /*html*/ `<!DOCTYPE html>
 			<html lang="en">
 			<head>
 				<meta charset="UTF-8">
@@ -129,14 +142,15 @@ export class SidepanelProvider implements vscode.WebviewViewProvider {
 				</script>
 			</body>
 			</html>`;
-    }
+  }
 }
 
 function getNonce() {
-    let text = '';
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (let i = 0; i < 32; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
 }
